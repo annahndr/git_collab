a1ae47f95c683bd45830d22a00db24ae0f69e231
Colin Farquhar <colin.farquhar@brightnetwork.co.uk>
2022-09-09 12:20:44 +0100

    changed print statement

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -2,7 +2,7 @@ public class Runner {
 
     public static void main(String[] args) {
         System.out.println("hello world!");
-        System.out.println("We're working on develop!");
+        System.out.println("We're working on a feature branch now!");
         System.out.println("we're making a pull request");
 
     }
